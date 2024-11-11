814225483f04d3cb2550fac163a2795041305c32
ktinson <monikai.tinson@verizon.com>
2024-11-11 14:05:50 -0600

    Added h2 DB

--- snippet/snippet/src/main/java/com/snippet/snippet/SnippetApplication.java
+++ snippet/snippet/src/main/java/com/snippet/snippet/SnippetApplication.java
@@ -2,12 +2,20 @@ package com.snippet.snippet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class SnippetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SnippetApplication.class, args);
+	}
+	@GetMapping("/")
+	public String apiRoot(){
+		return "This is the first page snippets";
 	}
 
 }
