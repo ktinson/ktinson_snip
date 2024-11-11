e43d4524d11c61bdafed0e89a90c9a079c2da0eb
ktinson <monikai.tinson@verizon.com>
2024-11-11 16:51:01 -0600

    need to fix the post

--- snippet/snippet/src/main/java/com/snippet/snippet/model/User.java
+++ snippet/snippet/src/main/java/com/snippet/snippet/model/User.java
@@ -16,36 +16,36 @@ public class User {
     private List<Snippet> snippets;
 
     // Getters and Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<Snippet> getSnippets() {
-        return snippets;
-    }
-
-    public void setSnippets(List<Snippet> snippets) {
-        this.snippets = snippets;
-    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public List<Snippet> getSnippets() {
+//        return snippets;
+//    }
+//
+//    public void setSnippets(List<Snippet> snippets) {
+//        this.snippets = snippets;
+//    }
 }
